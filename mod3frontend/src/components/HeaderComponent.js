17e78fce4cac910e875421f72122262f822b0f90
Malyika J <62891809+malyikaj@users.noreply.github.com>
2021-06-30 19:59:54 -0400

    Mod 3 upload

--- mod3frontend/src/components/HeaderComponent.js
+++ mod3frontend/src/components/HeaderComponent.js
@@ -14,10 +14,21 @@ class HeaderComponent extends React.Component {
         return(
             <div>
                 <header>
-                    <nav className="navbar navbar-expand-md nav-dark bg-dark">
+                <nav className="navbar navbar-light bg-light justify-content-between">
+                 <a href="/"className="navbar-brand" style={{marginLeft: "40px"}}>User List App</a>
+                 <form className="form-inline">
+                 {/* <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" ></input>
+                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
+                </form>
+                </nav>
+                </header>
+                <br></br> 
+                <br></br>
+                <br></br>
+
+                {/* <nav className="navbar navbar-expand-md nav-dark bg-dark">
                          <div><a href="/"className="navbar-brand">User Management App</a></div>
-                    </nav>
-                </header> 
+                    </nav> */}
             </div>
         )
             
